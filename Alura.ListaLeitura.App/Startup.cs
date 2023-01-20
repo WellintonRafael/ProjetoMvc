d9434d05c92d9204e9d3bc8068c9e4a5c1b5287f
WellintonSantosAgger <wellintonsantos@agger.com.br>
2023-01-20 16:39:10 -0300

    Update

--- Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura.App/Startup.cs
@@ -1,10 +1,73 @@
-﻿namespace Alura.ListaLeitura.App
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
 {
     public class Startup
     {
-        public void Configure()
+        public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting();
+        }
 
+        public void Configure(IApplicationBuilder app)
+        {
+            var builder = new RouteBuilder(app);
+            builder.MapRoute("Livros/ParaLer", LivrosParaLer);
+            builder.MapRoute("Livros/Lendo", LivrosLendo);
+            builder.MapRoute("Livros/ParaLer", LivrosLidos);
+
+            app.UseRouter(builder.Build());
+
+            //app.Run(Rotas);
+        }
+
+        public Task Rotas(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            var rotas = new Dictionary<string, RequestDelegate>
+            {
+                { "/Livros/ParaLer", LivrosParaLer },
+                { "/Livros/Lendo", LivrosLendo },
+                { "/Livros/Lidos", LivrosLidos }
+            };
+
+            if (rotas.ContainsKey(context.Request.Path))
+            {
+                var metodo = rotas[context.Request.Path];
+                return metodo.Invoke(context);
+
+            }
+
+            context.Response.StatusCode = 404;
+            return context.Response.WriteAsync("Rota inexistente");
+        }
+        public Task LivrosParaLer(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+
+            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+        }
+
+        public Task LivrosLendo(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            return context.Response.WriteAsync(_repo.Lendo.ToString());
+        }
+
+        public Task LivrosLidos(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            return context.Response.WriteAsync(_repo.Lidos.ToString());
         }
     }
 }
