26aacd04fc3edbfb9bddbc9a74c194f3fa949467
WellintonRafael <wellintonrafaelsantos@gmail.com>
2023-01-20 23:05:35 -0300

    Up

--- Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura.App/Startup.cs
@@ -3,8 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
-using System.Collections.Generic;
-using System.Net.Http;
+using System;
 using System.Threading.Tasks;
 
 namespace Alura.ListaLeitura.App
@@ -19,55 +18,53 @@ namespace Alura.ListaLeitura.App
         public void Configure(IApplicationBuilder app)
         {
             var builder = new RouteBuilder(app);
+            builder.MapRoute("Livros/Add", Add);
             builder.MapRoute("Livros/ParaLer", LivrosParaLer);
             builder.MapRoute("Livros/Lendo", LivrosLendo);
             builder.MapRoute("Livros/ParaLer", LivrosLidos);
 
             app.UseRouter(builder.Build());
 
-            //app.Run(Rotas);
+            app.Run(Error);
         }
 
-        public Task Rotas(HttpContext context)
+        public Task Error(HttpContext context)
         {
-            var _repo = new LivroRepositorioCSV();
-            var rotas = new Dictionary<string, RequestDelegate>
-            {
-                { "/Livros/ParaLer", LivrosParaLer },
-                { "/Livros/Lendo", LivrosLendo },
-                { "/Livros/Lidos", LivrosLidos }
-            };
-
-            if (rotas.ContainsKey(context.Request.Path))
-            {
-                var metodo = rotas[context.Request.Path];
-                return metodo.Invoke(context);
-
-            }
-
             context.Response.StatusCode = 404;
             return context.Response.WriteAsync("Rota inexistente");
         }
-        public Task LivrosParaLer(HttpContext context)
+
+        public async Task Add(HttpContext context)
         {
             var _repo = new LivroRepositorioCSV();
+            _repo.Incluir(new Negocio.Livro()
+            {
+                Autor = " RWELEAJDS",
+                Titulo = Guid.NewGuid().ToString()
+            });
 
-
-            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+            await context.Response.WriteAsync("BOA SAFADO REGISTRADO COM SUCESSO");
         }
 
-        public Task LivrosLendo(HttpContext context)
+        public async Task LivrosParaLer(HttpContext context)
         {
             var _repo = new LivroRepositorioCSV();
 
-            return context.Response.WriteAsync(_repo.Lendo.ToString());
+            await context.Response.WriteAsync(_repo.ParaLer.ToString());
         }
 
-        public Task LivrosLidos(HttpContext context)
+        public async Task LivrosLendo(HttpContext context)
         {
             var _repo = new LivroRepositorioCSV();
 
-            return context.Response.WriteAsync(_repo.Lidos.ToString());
+            await context.Response.WriteAsync(_repo.Lendo.ToString());
+        }
+
+        public async Task LivrosLidos(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            await context.Response.WriteAsync(_repo.Lidos.ToString());
         }
     }
 }
